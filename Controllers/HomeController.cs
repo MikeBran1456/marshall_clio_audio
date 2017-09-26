18714d020a09dbb9d6acc4eb2b1c9d460627ff8b
MikeBran <michaelbranard@gmail.com>
2017-09-26 16:01:36 -0400

    this may break everything

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace marshall_clio_audio.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This is a site assisting Clio.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact us";
 
             return View();
         }
