ec5749296d1ce37fcf6678d5ae8b99cebab40728
MikeBran <michaelbranard@gmail.com>
2017-09-26 14:46:59 -0400

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="marshall_clio_audio.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(marshall_clio_audio.Startup))]
+namespace marshall_clio_audio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
