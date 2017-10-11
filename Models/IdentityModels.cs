f2e926f49ba13501121b1840835b59739b35c562
MikeBran <michaelbranard@gmail.com>
2017-10-11 18:19:13 -0400

    connected to remote database hosted on appharbor

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace marshall_clio_audio.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<AudioFile> AudioFiles { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
