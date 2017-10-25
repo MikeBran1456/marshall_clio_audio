792c77dfc11278548a974b0226624a81c557dfae
MikeBran <michaelbranard@gmail.com>
2017-10-25 15:39:58 -0400

    fresh version from File upload change.

---
+++ marshall_clio_audio/Controllers/AudioFileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using marshall_clio_audio.Models;
+
+namespace marshall_clio_audio.Controllers
+{
+    public class AudioFileController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public AudioFileController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: AudioFile
+        public ActionResult Index()
+        {
+            var audioFile = _dbContext.AudioFiles.ToList();
+
+            return View(audioFile);
+        }
+    }
+}
---
+++ marshall_clio_audio/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using marshall_clio_audio.Models;
+
+namespace marshall_clio_audio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var ctx = new ApplicationDbContext();
+
+                int FileLength = file.ContentLength;
+                byte[] FileContents = new byte[FileLength];
+
+                file.InputStream.Read(FileContents, 0, FileLength);
+
+                AudioFile af = new AudioFile
+                {
+                    Name = "",
+                    WAVData = FileContents
+                };
+                ctx.AudioFiles.Add(af);
+
+                ctx.SaveChanges();
+
+                ViewBag.Message = "File upload was successful.";
+            } else
+            {
+                ViewBag.Message = "Could not upload the file.";
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var ctx = new ApplicationDbContext();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is a site assisting Clio.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact us";
+
+            return View();
+        }
+    }
+}
