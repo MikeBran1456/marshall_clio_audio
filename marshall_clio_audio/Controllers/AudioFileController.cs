fc880ae5fe23bb4fe5a27d9451078cea7c1da15d
muRoach <c.roach10@yahoo.com>
2017-10-25 22:21:56 -0400

    File table and deletion

--- marshall_clio_audio/Controllers/AudioFileController.cs
+++ marshall_clio_audio/Controllers/AudioFileController.cs
@@ -17,9 +17,32 @@ namespace marshall_clio_audio.Controllers
         // GET: AudioFile
         public ActionResult Index()
         {
-            var audioFile = _dbContext.AudioFiles.ToList();
+            var files = _dbContext.AudioFiles.ToList();
+             return View(files);
+        }
 
-            return View(audioFile);
+        public ActionResult Delete(int id)
+        {
+            var file = _dbContext.AudioFiles.SingleOrDefault(v => v.Id == id);
+            if(file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        public ActionResult doDelete(int id)
+        {
+            var file = _dbContext.AudioFiles.SingleOrDefault(v => v.Id == id);
+            if(file == null)
+            {
+                return HttpNotFound();
+            }
+
+            _dbContext.AudioFiles.Remove(file);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
